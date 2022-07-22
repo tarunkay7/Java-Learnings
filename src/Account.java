11d465a6583c85dc42a98f53db7b4780aecc0b45
Tarun Kesavan <tarunmenon383@gmail.com>
2022-07-22 11:23:18 +0530

    22/7/2022

--- src/Account.java
+++ src/Account.java
@@ -40,12 +40,16 @@ class AccountDemo
     public static void main(String[] args)
     {
         Account a1 = new Account();
+        Account a2 = new Account(2839,"loan",18239.00);
+        Account a3 = new Account();
        
         a1.accept();
         a1.display();
-        Account a2 = new Account(2839,"loan",18239.00);
         a2.accept();
         a2.display();
+        a3.accept();
+        a3.display();
+
         
         
     }
